500f874425c3f000de04f76d6a1c160b9452c8bf
kalille Overton <>
2021-04-15 21:54:42 -0400

    commit tree 49ae587903a2cbc0b8dc952e3a89507123a18a3d

---
+++ index.js
@@ -0,0 +1,14 @@
+// let divide = (a,b) => b/ a
+// //const divide = function(a,b){
+//      return a/b;
+//     }
+    
+//  function divide(a,b) {
+//      return 
+//  }
+const divide =function(){
+    return (2000 / 100)
+}
+const square = (x) => x ** 2;
+
+const add = (a,b)=> a + b;
